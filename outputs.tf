ac89d0474a95161816cabf81be68e4f1fa5cc24b
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:45:59 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "api_management_gateway_apis_id" {
   description = "Map of id values across all api_management_gateway_apis, keyed the same as var.api_management_gateway_apis"
-  value       = { for k, v in azurerm_api_management_gateway_api.api_management_gateway_apis : k => v.id }
+  value       = { for k, v in azurerm_api_management_gateway_api.api_management_gateway_apis : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "api_management_gateway_apis_api_id" {
   description = "Map of api_id values across all api_management_gateway_apis, keyed the same as var.api_management_gateway_apis"
-  value       = { for k, v in azurerm_api_management_gateway_api.api_management_gateway_apis : k => v.api_id }
+  value       = { for k, v in azurerm_api_management_gateway_api.api_management_gateway_apis : k => v.api_id if v.api_id != null && length(v.api_id) > 0 }
 }
 output "api_management_gateway_apis_gateway_id" {
   description = "Map of gateway_id values across all api_management_gateway_apis, keyed the same as var.api_management_gateway_apis"
-  value       = { for k, v in azurerm_api_management_gateway_api.api_management_gateway_apis : k => v.gateway_id }
+  value       = { for k, v in azurerm_api_management_gateway_api.api_management_gateway_apis : k => v.gateway_id if v.gateway_id != null && length(v.gateway_id) > 0 }
 }
 
